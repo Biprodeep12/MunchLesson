310043f00202ae6e731872704ce0920f29938997
Biprodeep12 <bipbose123@gmail.com>
2025-04-12 20:52:59 +0530

    fixma

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,9 @@
 import Dash from '@/components/dash';
 import Foot from '@/components/footer';
-import Nav from '@/components/nav';
 
 export default function DashPage() {
   return (
     <>
-      <Nav />
       <Dash />
       <Foot />
     </>
